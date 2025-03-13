6c6d8740d522dadd43a048ab532ad76c54ae6e0a
Lokesh <kapaselokesh@gmail.com>
2025-03-13 10:38:02 +0530

    Initial commit

---
+++ app/src/main/java/com/lokesh/google_map_integration/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.lokesh.google_map_integration
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.Circle
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.Polyline
+import com.lokesh.google_map_integration.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(){
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    private lateinit var mapsFragment: MapsFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+    }
+
+}
