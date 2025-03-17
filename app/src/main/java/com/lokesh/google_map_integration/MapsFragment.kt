5d0f2ec966464f8d16e2155c456f7edcda94bb8f
Lokesh <kapaselokesh@gmail.com>
2025-03-17 10:07:38 +0530

    commit 1

--- app/src/main/java/com/lokesh/google_map_integration/MapsFragment.kt
+++ app/src/main/java/com/lokesh/google_map_integration/MapsFragment.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,7 +20,9 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.PolygonOptions
 import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 
 class MapsFragment : Fragment() {
     private lateinit var mMap : GoogleMap
@@ -66,6 +69,92 @@ class MapsFragment : Fragment() {
                 .strokeWidth(10.0F)
                 .fillColor(Color.YELLOW)
                 .zIndex(45.0F))
+
+        polygon = mMap.addPolygon(
+            PolygonOptions()
+                .fillColor(Color.BLUE)
+                .strokeColor(Color.BLACK)
+                .strokeWidth(10.0f)
+                .zIndex(30.0f)
+                .add(LatLng(18.52,73.84))
+                .add(LatLng(19.07,72.87))
+                .add(LatLng(28.61,77.23))
+                .add(LatLng(12.97,77.59))
+        )
+        polyline = mMap.addPolyline(
+            PolylineOptions().add(LatLng(13.0674,80.2376))
+                .add(LatLng(22.5726,88.3639))
+                .add(LatLng(17.3871,78.4917))
+                .add(LatLng(23.0339,72.5850))
+                .add(LatLng(13.0674,80.2376))
+                .color(Color.CYAN)
+                .width(10.0f)
+
+        )
+    }
+
+    private fun setInfoWindowAdapter(){
+        //way 2 -- passing object of anonymous class by using object keyword in kotlin
+//        mMap.setOnMarkerDragListener(
+//            object  : GoogleMap.OnMarkerDragListener{
+//                override fun onMarkerDragStart(p0: Marker) {
+//                    TODO("Not yet implemented")
+//                }
+//                override fun onMarkerDrag(p0: Marker) {
+//                    TODO("Not yet implemented")
+//                }
+//                override fun onMarkerDragEnd(p0: Marker) {
+//                    TODO("Not yet implemented")
+//                }
+//            }
+//        )
+        //way 1
+        mMap.setOnMarkerDragListener(MyMarkerDragListener())
+
+    }
+    inner class MyMarkerDragListener : GoogleMap.OnMarkerDragListener{
+        override fun onMarkerDrag(p0: Marker) {
+            Log.e("tag", " marker drag ${p0.position.latitude} -- ${p0.position.longitude}")
+        }
+
+        override fun onMarkerDragEnd(p0: Marker) {
+            Log.e("tag", "marker drag end ${p0.position.latitude} -- ${p0.position.longitude}")
+        }
+
+        override fun onMarkerDragStart(p0: Marker) {
+            Log.e("tag", "marker drag start ${p0.position.latitude} -- ${p0.position.longitude}")
+        }
+    }
+
+    private fun setInfoWindowClickListener(){
+
+        //way 2
+//        mMap.setOnInfoWindowClickListener { marker->
+//            Log.e("tag", marker.title + marker.id)
+//
+//        }
+
+        //way 1
+        mMap.setOnInfoWindowClickListener(MyInfoWindowClickListener())
+    }
+
+    inner class MyInfoWindowClickListener : GoogleMap.OnInfoWindowClickListener{
+        override fun onInfoWindowClick(p0: Marker) {
+            Log.e("tag", "Title : " + p0.title + "--"+ "Id : "+ p0.id)
+        }
+    }
+
+
+
+    inner class MyInfoWindowAdapter : GoogleMap.InfoWindowAdapter{
+        override fun getInfoContents(p0: Marker): View? {
+            var infoWindowView = layoutInflater.inflate(R.layout.info_window,null)
+            return  infoWindowView
+        }
+
+        override fun getInfoWindow(p0: Marker): View? {
+            return null
+        }
     }
 
     private fun addMarkers(){
