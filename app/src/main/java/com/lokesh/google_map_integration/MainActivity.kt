97d6321e51ef54bdddcad1e68c895b68876cd02f
Lokesh <kapaselokesh@gmail.com>
2025-03-24 11:09:36 +0530

    commit 2

--- app/src/main/java/com/lokesh/google_map_integration/MainActivity.kt
+++ app/src/main/java/com/lokesh/google_map_integration/MainActivity.kt
@@ -6,17 +6,7 @@ import android.os.Bundle
 import android.provider.CalendarContract.Colors
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.Circle
-import com.google.android.gms.maps.model.CircleOptions
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.Polygon
-import com.google.android.gms.maps.model.Polyline
+
 import com.lokesh.google_map_integration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
